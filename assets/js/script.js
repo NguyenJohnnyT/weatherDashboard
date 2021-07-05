8da0d45c42a3577d93953a0329aecf24cad4b6eb
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-05 15:46:14 -0700

    add card construction function, pseudocode, an event handler for search click

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,23 @@
  * 10. Assign key values to appropriate elements and append them to the HTML [display final information to the user]
  **/
 
-cityInput = 'Fremont'
+cityInput = 'San Francisco'
 apiKey = '&units=imperial&appid=04b224ea6e7cb3656cbadc58a9e5d125'
 fiveDayAPI = 'https://api.openweathermap.org/data/2.5/forecast?q='
 fiveDayURL = fiveDayAPI.concat(cityInput, apiKey);
 
-const fetchLatLon = function (url) {
+var emojis = [ //values from main:
+    '☀️', //clear 0
+    '⛅', //clouds 1
+    '☁️', //clouds 2
+    '🌦', //drizzle 3
+    '🌧', //rain 4
+    '🌩', //thunderstorm 5
+    '❄️', //snow 6
+    '🌫', //fog 7
+];
+
+var fetchLatLon = function (url) {
     fetch(url)
     .then(function (response) {
         console.log(response);
@@ -33,7 +44,7 @@ const fetchLatLon = function (url) {
 }
 
 //from fiveDayURL, need to grab lat and longitude so we can get currentDayURL.
-const fetchData = function (latLon) {
+var fetchData = function (latLon) {
     var oneAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat='
     oneURL = oneAPI.concat(latLon[0], '&lon=', latLon[1], apiKey);
     fetch(oneURL)
@@ -42,13 +53,14 @@ const fetchData = function (latLon) {
         })
         .then(function (data) {
             console.log(data);
-            currentWeather = currentDay(data);
-            fiveDayForecast = forecastData(data);
+            currentWeather = currentDay(data); //an object
+            fiveDayForecast = forecastData(data); //an array of objects
             return [currentWeather, fiveDayForecast]
         })
 }
 
 function currentDay(data) {
+    //return an object that include weather data for day zero
     //dt, temp[day], humidity, wind_speed, uvi, weather general
     var dt = data.current['dt']*1000
     var currentTime = moment(dt).format('MMMM Do, YYYY')
@@ -64,6 +76,7 @@ function currentDay(data) {
 }
 
 function forecastData(data) {
+    //obtain an array of objects that include weather data for days 1-6 (five day forecast)
     //dt, temp[day], humidity, weather general
     var forecastArr = []
     for (i=1; i < 6; i++) {
@@ -82,20 +95,20 @@ function forecastData(data) {
 
 
 const start = async function () {
-    // const result = await fetchLatLon (fiveDayURL)
+
+    //fetchLatLon
     const latLon = await fetch(fiveDayURL)
     .then(function (response) {
-        console.log(response);
+        console.log('response', response);
         return response.json()
     })
     .then(function (data) {
-        // console.log(data);
-        // console.log(data.city.coord['lat'], data.city.coord['lon'])
         lat = data.city.coord['lat'];
         lon = data.city.coord['lon'];
-        // fetchData (lat, lon)
         return [lat,lon]
     })
+
+    //fetchData
     var oneAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat='
     oneURL = oneAPI.concat(latLon[0], '&lon=', latLon[1], apiKey);
     const result2 = await fetch(oneURL)
@@ -103,51 +116,75 @@ const start = async function () {
         return response.json()
     })
     .then(function (data) {
-        console.log(data);
-        currentWeather = currentDay(data);
-        fiveDayForecast = forecastData(data);
+        currentWeather = currentDay(data); //object of current day's 
+        fiveDayForecast = forecastData(data); //an array of objects
         return [currentWeather, fiveDayForecast]
     })
-    console.log('help', result2)
-    finalWeather = determineEmoji(result2)
+    finalWeather = determineEmoji(result2) //an array of objects from day 0 to 5
     console.log('finalWeather', finalWeather)
+    //Setting all values in the HTML PAGE 
+    // var divCard = document.createElement("div"); 
+    // divCard.setAttribute("class", "col card"); 
+    // var divTitle = document.createElement("h5"); 
+    // divTitle.setAttribute(); 
+    // divTitle.textContent = finalweather[index].date; 
+
+    // divCard.append(divTitle, p1, p2); 
+
+    // document.getElementsByClassName("row forecast").append(divCard); 
+
+    // document.getElementById("day1Title").textContent =  finalWeather[1].date;
+    
+    //SAVE IN the localstorage 
+   // "San Fran", "London", "Dublin" 
+   var previousList = localStorage.getItem("searchList") 
+    localStorage.setItem("searchList", JSON.stringify(cityInput))
     return finalWeather
 }
 
-weatherArr = start()
-console.log(weatherArr)
+function constructAdditionalCards (finalArr) {
+    $('#forecastContainer').empty();
+    $('#forecastContainer').append("<h4 style='height:20px'> 5-Day Forecast: </h4>");
+    for (i=1; i<finalArr.length; i++) { //start from day 1, end at day 5
+        var divCardEl = $('<div>'); //create column card
+        divCardEl.addClass('col card');
+        divCardEl.attr('id', ('forecast' + i));
+        
+        var cardBodyEl = $('<div>'); //create bootstrap Card element
+        cardBodyEl.addClass('Card-body');
 
-// const weatherData = async function (latLon) {
-//     var oneAPI = 'https://api.openweathermap.org/data/2.5/onecall?lat='
-//     oneURL = oneAPI.concat(latLon[0], '&lon=', latLon[1], apiKey);
-//     const result = await fetch(oneURL)
-//     .then(function (response) {
-//         return response.json()
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//         currentWeather = currentDay(data);
-//         fiveDayForecast = forecastData(data);
-//         return [currentWeather, fiveDayForecast]
-//     })
-//     console.log(result)
-//     return result
-// }
+        var h5El = $('<h5>'); //create h5 element for the forecast date
+        h5El.addClass('card-title');
+        h5El.attr('id', ('day' + i + 'Title'));
+        h5El.text(finalArr[i]['date']);
 
-// weatherData(coordinates)
+        var pEl1 = $('<p>'); //Emoji
+        var pEl2 = $('<p>'); //Temp
+        var pEl3 = $('<p>'); //Humidity
+        pEl1.addClass('card-text');
+        pEl2.addClass('card-text');
+        pEl3.addClass('card-text');
+        pEl1.text(finalArr[i]['emoji']);
+        pEl2.text('Temperature: ' + finalArr[i]['temp']);
+        pEl3.text('Humidity: ' + finalArray[i]['humidity']);
 
+        cardBodyEl.append(h5El, pEl1, pEl2, pEl3) //Append to card element each data input
+        divCardEl.append(cardBodyEl) //append to column the card element
+        $('#forecastContainer').append(divCardEl) //append the column to the row forecast
+    }
+}
 
 
 function determineEmoji (arr) { //iterate through the currentDay and forecast objects' 'weather' property to obtain a string value.  Switch case to give the 'emoji' property an emoji string.
-    temp1 = [arr[0]];
-    temp2 = arr[1];
-    finalArray = temp1.concat(temp2);
+    temp1 = [arr[0]]; //put current weather in an array
+    temp2 = arr[1]; //put the array of forecast objects into another array
+    finalArray = temp1.concat(temp2); //concat array of objects into one 'final' array
     console.log('finalArray', finalArray);
     for (i=0; i<finalArray.length; i++) {
         switch (finalArray[i]['weather'].toLowerCase()) {
-            case 'clear':
-                finalArray[i]['emoji'] = emojis[0];
-                break;
+            // case 'clear':
+            //     finalArray[i]['emoji'] = emojis[0];
+            //     break;
             case 'clouds':
                 finalArray[i]['emoji'] = emojis[1];
                 break;
@@ -166,18 +203,17 @@ function determineEmoji (arr) { //iterate through the currentDay and forecast ob
             case 'fog':
                 finalArray[i]['emoji'] = emojis[7]
                 break;
+            default:
+                finalArray[i]['emoji'] = emojis[0]
         }
     }
     return finalArray
 }
 
-emojis = [ //values from main:
-    '☀️', //clear 0
-    '⛅', //clouds 1
-    '☁️', //clouds 2
-    '🌦', //drizzle 3
-    '🌧', //rain 4
-    '🌩', //thunderstorm 5
-    '❄️', //snow 6
-    '🌫', //fog 7
-]
+
+
+
+
+$('#searchCity').on('click', function () {console.log('click!')}) //start
+weatherArr = start()
+console.log(weatherArr)
